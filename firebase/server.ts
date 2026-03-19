d838e841d18387b56e71b616b6fe1ce493edb57f
mgiannopoulos24 <79588074+mgiannopoulos24@users.noreply.github.com>
2026-03-19 15:34:27 +0200

    Update server.ts

--- firebase/server.ts
+++ firebase/server.ts
@@ -5,8 +5,8 @@ import admin from 'firebase-admin';
 const serviceAccount = {
   type: 'service_account',
   project_id: 'coding-club-c45e8',
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
   private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-  private_key: process.env.FIREBASE_PRIVATE_KEY,
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
   client_id: process.env.FIREBASE_CLIENT_ID,
   auth_uri: 'https://accounts.google.com/o/oauth2/auth',
