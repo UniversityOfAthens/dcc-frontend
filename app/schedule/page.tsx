c0094396cecac9965982dee2f5c190ffc2d754d8
mgiannopoulos24 <79588074+mgiannopoulos24@users.noreply.github.com>
2026-05-01 11:11:10 +0300

    Invert only if user doesnt user Dark Reader

--- app/schedule/page.tsx
+++ app/schedule/page.tsx
@@ -2,7 +2,7 @@ export default function Schedule() {
   return (
     <div className="h-[90vh] min-h-screen">
       <iframe
-        className="w-full h-full p-4 [filter:invert(90%)_hue-rotate(180deg)]"
+        className="w-full h-full p-4 [filter:invert(90%)_hue-rotate(180deg)] dark:[filter:none]"
         src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=Europe%2FAthens&showPrint=0&src=NzU4ZTlmY2NiMGM0NjAxYmIzMzNmYWI1MjZkNGUzMzIyNGQ0YTBjYWI4NjllZTIwMWEyYWYzNzkxMTIzNDUwYkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23AD1457"
       ></iframe>
     </div>
